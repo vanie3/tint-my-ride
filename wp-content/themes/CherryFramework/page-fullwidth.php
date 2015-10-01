446e11222d21a5ffb27a9d0cfa184d97bea90286
vanie3 <vansing3@gmail.com>
2015-10-01 00:19:10 -0700

    needed to remove title from contact page

--- wp-content/themes/CherryFramework/page-fullwidth.php
+++ wp-content/themes/CherryFramework/page-fullwidth.php
@@ -11,7 +11,7 @@ get_header(); ?>
 			<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-wrapper-file="page-fullwidth.php" data-motopress-wrapper-type="content">
 				<div class="row">
 					<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="static" data-motopress-static-file="static/static-title.php">
-						<?php get_template_part("static/static-title"); ?>
+<!--						--><?php //get_template_part("static/static-title"); ?>
 					</div>
 				</div>
 				<div id="content" class="row">
